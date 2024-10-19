de6eeb650ef86f66cea4204e36239f00375015c5
uscabayaosj <uscabayaosj@addu.edu.ph>
2024-10-19 17:49:26 +0000

    fixed config.py discrepancy

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///journal.db'
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///journal.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'static/uploads'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://username:password@    
+    
